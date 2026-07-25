c23abac65e86a71d84c4d9899b6ba743711b3335
YAT?STATS <pcdaction@gmail.com>
2026-07-25 13:36:55 -0700

    Build Sina first-pass site structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Shop from './components/Shop';
+import Gallery from './components/Gallery';
 import ArtistSection from './components/ArtistSection';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -13,6 +14,7 @@ export default function App() {
       <main>
         <Hero />
         <Shop />
+        <Gallery />
         <ArtistSection />
         <Contact />
       </main>
