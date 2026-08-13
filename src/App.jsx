a1118ad651e098d4c678ccf3d8899cb0a3f8a934
YAT?STATS <pcdaction@gmail.com>
2026-08-12 22:40:06 -0700

    Route Sina site through Lifted-style page system

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import Nav from './components/Nav.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
-import Story from './pages/Story.jsx'
-import Collections from './pages/Collections.jsx'
-import Collaborate from './pages/Collaborate.jsx'
-import Wholesale from './pages/Wholesale.jsx'
-import Shop from './pages/Shop.jsx'
-import Schedule from './pages/Schedule.jsx'
-import NotFound from './pages/NotFound.jsx'
+import {
+  Home,
+  Story,
+  Collections,
+  Collaborate,
+  Wholesale,
+  Shop,
+  Schedule,
+  NotFound,
+} from './pages/SinaPages.jsx'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
@@ -17,29 +17,19 @@ function ScrollToTop() {
   return null
 }
 
-function PublicLayout({ children }) {
-  return (
-    <>
-      <Nav />
-      <main>{children}</main>
-      <Footer />
-    </>
-  )
-}
-
 export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<PublicLayout><Home /></PublicLayout>} />
-        <Route path="/story" element={<PublicLayout><Story /></PublicLayout>} />
-        <Route path="/collections" element={<PublicLayout><Collections /></PublicLayout>} />
-        <Route path="/collaborate" element={<PublicLayout><Collaborate /></PublicLayout>} />
-        <Route path="/wholesale" element={<PublicLayout><Wholesale /></PublicLayout>} />
-        <Route path="/shop" element={<PublicLayout><Shop /></PublicLayout>} />
-        <Route path="/schedule" element={<PublicLayout><Schedule /></PublicLayout>} />
-        <Route path="*" element={<PublicLayout><NotFound /></PublicLayout>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/story" element={<Story />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/collaborate" element={<Collaborate />} />
+        <Route path="/wholesale" element={<Wholesale />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
