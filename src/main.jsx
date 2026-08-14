c26f091479c1e0ddeecaae04fdd79cd2deae446a
YAT?STATS <pcdaction@gmail.com>
2026-08-14 10:13:56 -0700

    Show living mosaic as homepage hero composition

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles/global.css'
 import './styles/logo-layout-fixes.css'
 import './styles/home-hero-mosaic-preview.css'
 import './styles/living-mosaic-composition-overrides.css'
+import './styles/living-mosaic-top-correction.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
